8ed94f4c6a73c057fb053221c4da649bbf7f61e9
Franek <franekkaminski@gmail.com>
2026-01-03 23:19:28 +0100

    Flexible types

---
+++ 1_Getting_Started/flexible-types.ts
@@ -0,0 +1,13 @@
+let grade: any = 36;
+
+grade = '37';
+grade = false
+grade = {}
+grade = []
+
+// use any as last resort, try to avoid it
+
+let number: string | number = 1;
+
+number = '1';
+number = 'one';
