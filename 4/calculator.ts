1ecab23092223e4d8894a6d4c2affde765fc9abe
Franek <franekkaminski@gmail.com>
2026-01-07 22:28:21 +0100

    Demo project setup

---
+++ 4/calculator.ts
@@ -0,0 +1,15 @@
+// data:
+// initial amount
+// annual contribution
+// expected return
+// duration
+
+function calculateInvestment(data) {} // => result []
+
+function printResult(results) {
+    // print (output) the result data
+}
+
+const results = calculateInvestment(...)
+
+printResult(results)
