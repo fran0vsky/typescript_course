47f7e6ae40d42c323467135464bf5b4f5a5cf0b1
Franek <franekkaminski@gmail.com>
2026-01-08 22:26:12 +0100

    Continuation

--- 4/calculator.ts
+++ 4/calculator.ts
@@ -4,7 +4,36 @@
 // expected return
 // duration
 
-function calculateInvestment(data) {} // => result []
+type InvestmentData = {
+    initialAmount: number;
+    annualContribution: number;
+    expectedReturn: number;
+    duration: number;
+};
+
+type InvestmentResult = {
+    year: string;
+    totalAmount: number;
+    totalContributions: number;
+    totalInterestEarned: number;
+};
+
+type CalculationResult = InvestmentResult[] | string;
+
+function calculateInvestment(data: InvestmentData): CalculationResult {
+    const { initialAmount, annualContribution, duration, expectedReturn } = data;
+
+    if (initialAmount < 0) {
+        return 'Initial investment amount must be at least zero.'
+    }
+
+    if (duration <= 0){
+        return 'No valid amount of years provided.'
+    }
+    if (expectedReturn < 0){
+        return 'Expected return must be at least zero.'
+    }
+}
 
 function printResult(results) {
     // print (output) the result data
