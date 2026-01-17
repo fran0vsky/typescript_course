ae5eea739b197a5086a81d7675100062349af395
Franek <franekkaminski@gmail.com>
2026-01-17 22:25:27 +0100

    Setting values with Setters

--- 6/advancedClass.ts
+++ 6/advancedClass.ts
@@ -7,5 +7,24 @@ class User {
     }
 }
 
+// Setters
+//
+// class User {
+//     private firstName: string = '';
+//     private lastName: string = '';
+//
+//     set firstName(value: string) { 
+//       if (name.trim() === '') {
+//         throw new Error('Invalid name');
+//}
+//       this.firstName = value;
+//     }
+//
+//     get fullName() {
+//         return this.firstName + ' ' + this.lastName;
+//
+//     }
+// }
+
 const franek = new User('Franek', 'Kaminski');
 console.log(franek.fullName);
