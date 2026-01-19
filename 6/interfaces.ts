69b062c4ac1cd60d6b15b75219ef0a3c285e6204
Franek <franekkaminski@gmail.com>
2026-01-19 21:58:07 +0100

    Interfaces As Object Types

--- 6/interfaces.ts
+++ 6/interfaces.ts
@@ -3,5 +3,18 @@ interface Authenticable {
     password: string;
 
     login(): void;
-    login(): void;
+    logout(): void;
+}
+
+let user: Authenticable;
+
+user = {
+    email: 'test@example.com',
+    password: 'abc1',
+    login() {
+        // reach out to the database, check credentials, create a session
+    },
+    logout() {
+        // clear the session
+    }
 }
