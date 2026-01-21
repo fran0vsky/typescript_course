14ff64a2003b452e0dce5a2091e59d13b09a088d
Franek <franekkaminski@gmail.com>
2026-01-21 22:50:43 +0100

    Discriminated Unions

--- 7/guards.ts
+++ 7/guards.ts
@@ -1,19 +1,24 @@
-type FileSource = { path: string };
+type FileSource = { type: "file"; path: string };
 const fileSource: FileSource = {
+  type: "file",
   path: "some/path/to/file.csv",
 };
 
-type DBSource = { connectionUrl: string };
+type DBSource = { type: "db"; connectionUrl: string };
 const dbSource: DBSource = {
+  type: "db",
   connectionUrl: "some-connection-url",
 };
 
 type Source = FileSource | DBSource;
 
 function loadData(source: Source) {
-  if ("path" in source) {
+  // if ("path" in source) {
+  if (source.type === "file") {
+    //source.path;
     // source.path; => use that to open the file
     return;
   }
+  source.connectionUrl;
   // source.connectionUrl; => use that to connect to the database
 }
