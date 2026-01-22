24f3e2bcb71767f8d55a41ed24c1e9cfe597a0e5
Franek <franekkaminski@gmail.com>
2026-01-22 22:40:51 +0100

    Type Guard via "instanceof"

--- 7/guards.ts
+++ 7/guards.ts
@@ -1,24 +1,53 @@
-type FileSource = { type: "file"; path: string };
+type FileSource = { type: 'file'; path: string };
 const fileSource: FileSource = {
-  type: "file",
-  path: "some/path/to/file.csv",
+  type: 'file',
+  path: 'some/path/to/file.csv',
 };
 
-type DBSource = { type: "db"; connectionUrl: string };
+type DBSource = { type: 'db', connectionUrl: string };
 const dbSource: DBSource = {
-  type: "db",
-  connectionUrl: "some-connection-url",
+  type: 'db',
+  connectionUrl: 'some-connection-url',
 };
 
 type Source = FileSource | DBSource;
 
 function loadData(source: Source) {
-  // if ("path" in source) {
-  if (source.type === "file") {
-    //source.path;
+  // if ('path' in source) {
+  if (source.type === 'file') {
+    // source.path
     // source.path; => use that to open the file
     return;
   }
-  source.connectionUrl;
-  // source.connectionUrl; => use that to connect to the database
+  // source.connectionUrl; => to reach out to database
+}
+
+class User {
+  constructor(public name: string) {}
+
+  join() {
+    // ...
+  }
+}
+
+class Admin {
+  constructor(permissions: string[]) {}
+
+  scan() {
+    // ...
+  }
+}
+
+const user = new User('Max');
+const admin = new Admin(['ban', 'restore']);
+
+type Entity = User | Admin;
+
+function init(entity: Entity) {
+  if (entity instanceof User) {
+   entity.join();
+   return;
+ }
+
+ entity.scan();
 }
