f34945009bee97108daac0847531697760a565bc
Franek <franekkaminski@gmail.com>
2026-01-26 22:47:39 +0100

    Constant Types with "as const"

--- 7/indexTypes.ts
+++ 7/indexTypes.ts
@@ -9,3 +9,7 @@ let store: DataStore = {};
 store.id = 5;
 store.isOpen = false;
 store.name= 'Franek';
+
+let roles = ['admin', 'guest', 'editor'] as const;
+// roles.push('franek');
+const firstRole = roles[0];
