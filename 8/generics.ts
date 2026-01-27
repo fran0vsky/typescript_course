5c540724ac349ff1e0d3efa00cf2b6dab63f9222
Franek <franekkaminski@gmail.com>
2026-01-27 23:36:30 +0100

    Creating & Using a Generic Type

--- 8/generics.ts
+++ 8/generics.ts
@@ -1,1 +1,11 @@
 let names: Array<string> = ['Franek', 'Maciek']
+
+type DataStore<T> = {
+  [key: string]:  T
+}
+
+let store: DataStore<string | boolean> = {};
+store.name = 'Franek';
+store.isInstructor = true;
+
+let nameStore: DataStore<string> = {};
