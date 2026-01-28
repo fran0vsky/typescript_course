0f4febad7f131e6dd0805a0aeffee263ac1ac8ca
Franek <franekkaminski@gmail.com>
2026-01-28 21:55:17 +0100

    Working with Multiple Generic Parameters

--- 8/generics.ts
+++ 8/generics.ts
@@ -1,17 +1,17 @@
-let names: Array<string> = ['Franek', 'Maciek']
+let names: Array<string> = ["Franek", "Maciek"];
 
 type DataStore<T> = {
-  [key: string]:  T
-}
+  [key: string]: T;
+};
 
 let store: DataStore<string | boolean> = {};
-store.name = 'Franek';
+store.name = "Franek";
 store.isInstructor = true;
 
 let nameStore: DataStore<string> = {};
 
-function merge<T>(a: T, b: T) {
+function merge<T, U>(a: T, b: U) {
   return [a, b];
 }
 
-const ids = merge(1, 2);
+const ids = merge(1, "Franek");
